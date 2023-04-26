9d56b66891c522effb23ef0da3064744498d08af
Lucjan Wilczewski <lucjan.wilczewski@solveq.io>
2023-04-26 23:25:58 +0200

    feat(api-gateway): introduce rate limiting

--- apps/registrations-service/src/app/app.controller.ts
+++ apps/registrations-service/src/app/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller } from '@nestjs/common';
 
 import { MessagePattern } from "@nestjs/microservices";
-import { delay, of } from "rxjs";
+import { of } from "rxjs";
 
 @Controller()
 export class AppController {
   @MessagePattern({ cmd: "ping" })
   ping() {
-    return of("pong").pipe(delay(1000));
+    return of("pong");
   }
 }
--- apps/web-api/src/app/app.module.ts
+++ apps/web-api/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from "@nestjs/microservices";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ClientsModule.register([
       {
         name: "EVENTS_SERVICE",
@@ -26,6 +32,12 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
     ])
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    AppService
+  ],
 })
 export class AppModule {}
