bbec443eb24e87e38c118c5830eab0a37c1d9d09
Lucjan Wilczewski <lucjan.wilczewski@solveq.io>
2023-04-27 20:12:55 +0200

    feat(api-gateway): introduce request logging

--- apps/web-api/src/app/app.module.ts
+++ apps/web-api/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { LoggerModule } from "nestjs-pino";
 import { ClientsModule, Transport } from "@nestjs/microservices";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { APP_GUARD } from "@nestjs/core";
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { APP_GUARD } from "@nestjs/core";
       ttl: 60,
       limit: 10,
     }),
+    LoggerModule.forRoot(),
     ClientsModule.register([
       {
         name: "EVENTS_SERVICE",
--- apps/web-api/src/main.ts
+++ apps/web-api/src/main.ts
@@ -3,20 +3,28 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger, VersioningType } from "@nestjs/common";
+import { VersioningType } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
+import * as responseTime from 'response-time';
 
 import { AppModule } from './app/app.module';
-
-const logger = new Logger('api-gateway')
+import { Logger } from "nestjs-pino";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const logger = app.get<Logger>(Logger)
+
+  app.use(responseTime())
+  app.useLogger(logger)
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   logger.log(
